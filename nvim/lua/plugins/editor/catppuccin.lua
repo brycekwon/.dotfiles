b243e5a0b8d5855cc1e76d27a5491eb5725f68d6
Bryce Kwon <signer@brycekwon.dev>
2024-03-19 09:00:31 -0700

    Update theme integrations

--- nvim/lua/plugins/editor/catppuccin.lua
+++ nvim/lua/plugins/editor/catppuccin.lua
@@ -3,8 +3,52 @@ return {
     name = 'catppuccin',
     opts = {
         integration = {
+            alpha = true,
             nvimtree = true,
             treesitter = true,
+            gitsigns = true,
+            cmp = true,
+            harpoon = true,
+            indent_blankline = true,
+            mason = true,
+            neogit = true,
+            telescope = {
+                enabled = true,
+            },
+            native_lsp = {
+                enabled = true,
+                virtual_text = {
+                    errors = {
+                        "italic"
+                    },
+                    hints = {
+                        "italic"
+                    },
+                    warnings = {
+                        "italic"
+                    },
+                    information = {
+                        "italic"
+                    },
+                },
+                underlines = {
+                    errors = {
+                        "underline"
+                    },
+                    hints = {
+                        "underline"
+                    },
+                    warnings = {
+                        "underline"
+                    },
+                    information = {
+                        "underline"
+                    },
+                },
+                inlay_hints = {
+                    background = true,
+                },
+            },
         },
     },
     lazy = false,
