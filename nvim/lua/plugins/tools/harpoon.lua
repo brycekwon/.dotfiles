fa29b412011cc1f2c5328386f185c29a29fe702d
Bryce Kwon <signer@brycekwon.dev>
2024-03-19 08:57:47 -0700

    Update neovim keymaps

--- nvim/lua/plugins/tools/harpoon.lua
+++ nvim/lua/plugins/tools/harpoon.lua
@@ -11,10 +11,10 @@ return {
     keys = {
         { '<leader>a', function() require('harpoon'):list():append() end, mode='n' },
         { '<leader>z', function() local harpoon = require('harpoon') harpoon.ui:toggle_quick_menu(harpoon:list()) end, mode='n' },
-        { '<leader>1', function() require('harpoon'):list():select(1) end, mode='n' },
-        { '<leader>2', function() require('harpoon'):list():select(2) end, mode='n' },
-        { '<leader>3', function() require('harpoon'):list():select(3) end, mode='n' },
-        { '<leader>4', function() require('harpoon'):list():select(4) end, mode='n' },
-        { '<leader>5', function() require('harpoon'):list():select(5) end, mode='n' },
+        { '<C-1>', function() require('harpoon'):list():select(1) end, mode='n' },
+        { '<C-2>', function() require('harpoon'):list():select(2) end, mode='n' },
+        { '<C-3>', function() require('harpoon'):list():select(3) end, mode='n' },
+        { '<C-4>', function() require('harpoon'):list():select(4) end, mode='n' },
+        { '<C-5>', function() require('harpoon'):list():select(5) end, mode='n' },
     }
 }
