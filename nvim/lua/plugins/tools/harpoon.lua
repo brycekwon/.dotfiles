50bc9f39bab55a4fee5ed469aeca0d434bdeb1f5
Bryce Kwon <signer@brycekwon.dev>
2024-03-21 00:33:31 -0700

    Update keymaps for tabs and harpoon

--- nvim/lua/plugins/tools/harpoon.lua
+++ nvim/lua/plugins/tools/harpoon.lua
@@ -11,10 +11,14 @@ return {
     keys = {
         { '<leader>a', function() require('harpoon'):list():append() end, mode='n' },
         { '<leader>z', function() local harpoon = require('harpoon') harpoon.ui:toggle_quick_menu(harpoon:list()) end, mode='n' },
-        { '<C-1>', function() require('harpoon'):list():select(1) end, mode='n' },
-        { '<C-2>', function() require('harpoon'):list():select(2) end, mode='n' },
-        { '<C-3>', function() require('harpoon'):list():select(3) end, mode='n' },
-        { '<C-4>', function() require('harpoon'):list():select(4) end, mode='n' },
-        { '<C-5>', function() require('harpoon'):list():select(5) end, mode='n' },
+        { '<leader>1', function() require('harpoon'):list():select(1) end, mode='n' },
+        { '<leader>2', function() require('harpoon'):list():select(2) end, mode='n' },
+        { '<leader>3', function() require('harpoon'):list():select(3) end, mode='n' },
+        { '<leader>4', function() require('harpoon'):list():select(4) end, mode='n' },
+        { '<leader>5', function() require('harpoon'):list():select(5) end, mode='n' },
+        { '<leader>6', function() require('harpoon'):list():select(6) end, mode='n' },
+        { '<leader>7', function() require('harpoon'):list():select(7) end, mode='n' },
+        { '<leader>8', function() require('harpoon'):list():select(8) end, mode='n' },
+        { '<leader>9', function() require('harpoon'):list():select(9) end, mode='n' },
     }
 }
